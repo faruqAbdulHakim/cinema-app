88a63b9ae578c2504c96e898ca72d56b0648add3
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2021-07-24 18:14:25 +0700

    project init

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>CINEMA | Stream TV and Movies Live and Online</title>
+      </Head>
+      <h1>CINEMA | Stream TV and Movies Live and Online</h1>
+    </div>
+  );
+}
